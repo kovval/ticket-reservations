c053d94f38f0f2e08873770eeb89f27464e7c94d
MariAntonina <maryhuzarek@gmail.com>
2017-11-09 20:49:08 +0100

    Add userRole

--- src/main/java/com/github/java4wro/user/model/userRole.java
+++ src/main/java/com/github/java4wro/user/model/userRole.java
@@ -3,6 +3,6 @@ package com.github.java4wro.user.model;
 /**
  * Created by admin on 09.11.2017.
  */
-public enum userEnum {
-    USER, INTERNAL, ADMIN,
+public enum userRole {
+    USER, INTERNAL, ADMIN
 }
