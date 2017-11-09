cde09464905115a68f4d2ca9966dd6768848bd0b
esiu06 <pawel.esmanowicz@wp.pl>
2017-11-09 20:38:25 +0100

    ticket

--- src/main/java/com/github/java4wro/ticket/Ticket.java
+++ src/main/java/com/github/java4wro/ticket/Ticket.java
@@ -1,39 +1,11 @@
 package com.github.java4wro.ticket;
 
 
-import javax.persistence.*;
+import javax.persistence.Entity;
 
 @Entity
 public class Ticket {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    public String id;
-    @ManyToOne
-    public Long rov;
-    public Long place;
+    public String name;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public Long getRov() {
-        return rov;
-    }
-
-    public void setRov(Long rov) {
-        this.rov = rov;
-    }
-
-    public Long getPlace() {
-        return place;
-    }
-
-    public void setPlace(Long place) {
-        this.place = place;
-    }
 }
