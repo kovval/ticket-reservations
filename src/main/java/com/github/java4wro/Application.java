2d512e8413bff894343227116eb340a759c7deaf
xenteros <adam@gurgul.com.pl>
2017-11-09 19:57:54 +0100

    Add basic dependencies and psvm (#1)

--- src/main/java/com/github/java4wro/Application.java
+++ src/main/java/com/github/java4wro/Application.java
@@ -1,4 +1,12 @@
 package com.github.java4wro;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
