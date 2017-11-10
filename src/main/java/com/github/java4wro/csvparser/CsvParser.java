0a1f4af1ac2483f6788a73a3a30a6f40f2f1ec6a
slavo <szweds@poczta.onet.pl>
2017-11-10 18:31:42 +0100

    prepare bufferreader

--- src/main/java/com/github/java4wro/csvparser/CsvParser.java
+++ src/main/java/com/github/java4wro/csvparser/CsvParser.java
@@ -1,5 +1,26 @@
 package com.github.java4wro.csvparser;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class CsvParser {
+    private File file;
+
+    public CsvParser(File file) throws FileNotFoundException {
+        this.file = file;
+    }
+
+    FileReader fis = new FileReader(file);
+    BufferedReader br = new BufferedReader(fis);
+
+
+    public void readFile(){
+
+
+
+
+    }
 }
