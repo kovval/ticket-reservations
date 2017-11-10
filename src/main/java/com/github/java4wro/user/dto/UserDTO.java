1ead9d7f63fe10c9cf0b571219b33e922987393b
Lukas <lukas.wojtkowiak@gmail.com>
2017-11-10 18:28:39 +0100

    Add userRepository, userDto

---
+++ src/main/java/com/github/java4wro/user/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package com.github.java4wro.user.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+
+    private String email;
+    private String password;
+    private String uuid;
+}
