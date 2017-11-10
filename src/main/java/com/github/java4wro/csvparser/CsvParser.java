a06f311648acac54a7e3ad2c43dc003ee31e8043
slavo <szweds@poczta.onet.pl>
2017-11-10 20:16:35 +0100

    prepare HashMap with available seats

--- src/main/java/com/github/java4wro/csvparser/CsvParser.java
+++ src/main/java/com/github/java4wro/csvparser/CsvParser.java
@@ -1,26 +1,37 @@
 package com.github.java4wro.csvparser;
-
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.util.HashMap;
 
 public class CsvParser {
     private File file;
-
-    public CsvParser(File file) throws FileNotFoundException {
-        this.file = file;
-    }
-
-    FileReader fis = new FileReader(file);
+    private HashMap<String, Float> hall_1 = new HashMap<>();
+    private String line;
+    private final String UTF8_BOM = "\uFEFF";
+    FileReader fis = new FileReader("src/main/java/com.github.java4wro/csvparser/hall_1.csv");
     BufferedReader br = new BufferedReader(fis);
-
-
-    public void readFile(){
-
-
-
-
+    public CsvParser() throws FileNotFoundException {
+    }
+    public void readFile() {
+        int row = 1;
+        try {
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith(UTF8_BOM)) {
+                    line = line.substring(1);
+                }
+                String[] singleRow = line.split(",");
+                for (int i = 0; i < singleRow.length; i++) {
+                    if (Float.valueOf(singleRow[i]) > 0) {
+                        Character r = (char) (row + 64);
+                        StringBuilder sB = new StringBuilder();
+                        sB.append(r);
+                        sB.append(String.valueOf(i + 1));
+                        hall_1.put(sB.toString(), Float.valueOf(singleRow[i]));
+                    }
+                }
+                row++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
