3ef2892b306fba2297a732a16a7f55cab43ab6be
esiu06 <pawel.esmanowicz@wp.pl>
2017-11-13 19:45:40 +0100

    addEvent

---
+++ src/main/java/com/github/java4wro/event/service/EventService.java
@@ -0,0 +1,10 @@
+package com.github.java4wro.event.service;
+
+import com.github.java4wro.dto.EventDTO;
+import com.github.java4wro.event.Event;
+
+public interface EventService {
+
+    EventDTO addEvent (EventDTO eventDTO);
+
+}
