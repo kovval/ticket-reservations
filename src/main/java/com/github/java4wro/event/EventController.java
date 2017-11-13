5ee58568bcc614626da3e7675c93f39c06cd950a
Marcin <marcinster@gmail.com>
2017-11-13 20:54:18 +0100

    Add add-event form

--- src/main/java/com/github/java4wro/event/EventController.java
+++ src/main/java/com/github/java4wro/event/EventController.java
@@ -3,10 +3,7 @@ package com.github.java4wro.event;
 import com.github.java4wro.dto.EventDTO;
 import com.github.java4wro.event.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by RENT on 2017-11-13.
@@ -18,7 +15,7 @@ public class EventController {
     @Autowired
     private EventService eventService;
 
-    @PutMapping("/add")
+    @PostMapping("/add")
     private EventDTO add(@RequestBody EventDTO eventDTO ){
         return eventService.addEvent(eventDTO);
     }
