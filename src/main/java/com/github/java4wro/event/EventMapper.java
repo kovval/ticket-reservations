8d8bb21b76bed0ed0301d07fa44ed41f29add918
esiu06 <pawel.esmanowicz@wp.pl>
2017-11-14 20:11:42 +0100

    AddEvent

--- src/main/java/com/github/java4wro/event/EventMapper.java
+++ src/main/java/com/github/java4wro/event/EventMapper.java
@@ -5,21 +5,28 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.ReportingPolicy;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by RENT on 2017-11-13.
  */
-
-@Mapper(componentModel = "spring" ,unmappedTargetPolicy = ReportingPolicy.ERROR)
+@Mapper(componentModel = "spring" ,unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface EventMapper {
 
     @Mappings({
             @Mapping(source = "title", target = "eventName"),
             @Mapping(source = "description", target = "eventDescription"),
             @Mapping(source = "basicPrice", target = "eventPrice"),
-            @Mapping(source = "dateTime", target = "eventDateTime")
+            @Mapping(source = "dateTime", target = "dateAndTime")
     })
-
     EventDTO toEventDTO(Event event);
 
+    @Mappings({
+            @Mapping(source = "dateAndTime", target = "dateTime"),
+            @Mapping(source = "eventName", target = "title"),
+            @Mapping(source = "eventDescription", target = "description"),
+            @Mapping(source = "eventPrice", target = "basicPrice"),
+
+    })
+    Event toEvent (EventDTO eventDTO);
 }
