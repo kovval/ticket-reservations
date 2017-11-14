55e9f14eabdfb25f98428837e747f189efe66ca7
Wojt <w.g.michalik@gmail.com>
2017-11-14 20:48:32 +0100

    prepare working Liquibase with csv file

--- src/main/java/com/github/java4wro/csvparser/model/Hall.java
+++ src/main/java/com/github/java4wro/csvparser/model/Hall.java
@@ -1,11 +1,9 @@
 package com.github.java4wro.csvparser.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "hall")
 public class Hall {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
