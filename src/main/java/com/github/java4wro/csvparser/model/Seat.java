6749c4d15182026a4a44f50f0944409fe4fd5149
xenteros <adam@gurgul.com.pl>
2017-11-15 21:01:45 +0100

    Add liquibase and hall upload (#8)
    * Configure liquibase,
    * Add endpoint for uploading seats

---
+++ src/main/java/com/github/java4wro/csvparser/model/Seat.java
@@ -0,0 +1,30 @@
+package com.github.java4wro.csvparser.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "hall")
+public class Seat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    private String seat;
+    private Float value;
+
+    public String getSeat() {
+        return seat;
+    }
+
+    public void setSeat(String seat) {
+        this.seat = seat;
+    }
+
+    public Float getValue() {
+        return value;
+    }
+
+    public void setValue(Float value) {
+        this.value = value;
+    }
+}
