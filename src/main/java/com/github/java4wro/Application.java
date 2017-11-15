ce0d85c6ed2d56c72d931a75af8fa32039d771e7
Paweł Dzierżęga <pawel.dzierzega@outlook.com>
2017-11-15 19:56:40 +0100

    Change properties

--- src/main/java/com/github/java4wro/Application.java
+++ src/main/java/com/github/java4wro/Application.java
@@ -3,9 +3,15 @@ package com.github.java4wro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.sql.DataSource;
 
 
 @EnableScheduling
@@ -13,6 +19,13 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableJpaRepositories
 @EntityScan(basePackageClasses = {Application.class, Jsr310JpaConverters.class})
 public class Application {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
