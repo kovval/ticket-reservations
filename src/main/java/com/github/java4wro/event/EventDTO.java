f3b21bebd77866ec502ace613bbe045a23b99258
RENT-Komputer\RENT <m.bandzarzewski@gmail.com>
2017-11-15 17:36:17 +0100

    Merge branch 'grupa2_Model' of C:\Users\RENT\Desktop\ticket-reservations with conflicts.

--- src/main/java/com/github/java4wro/event/EventDTO.java
+++ src/main/java/com/github/java4wro/event/EventDTO.java
@@ -1,59 +1,18 @@
-package com.github.java4wro.event;
+package com.github.java4wro.dto;
 
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 public class EventDTO {
 
-    private BigDecimal eventPrice;
+    private Integer eventPrice;
     private String eventName;
     private String eventDescription;
-//    private LocalDateTime eventDateTime;
-    private String dateAndTime;
-
- 
-
-    public void setDateAndTime(String dateAndTime) {
-        this.dateAndTime = dateAndTime;
-    }
+    private LocalDateTime eventDateTime;
 
 
-    public void setEventPrice(BigDecimal eventPrice) {
-        this.eventPrice = eventPrice;
-    }
-
-    public String getEventName() {
-        return eventName;
-    }
-
-    public void setEventName(String eventName) {
-        this.eventName = eventName;
-    }
-
-    public String getEventDescription() {
-        return eventDescription;
-    }
-
-    public void setEventDescription(String eventDescription) {
-        this.eventDescription = eventDescription;
-    }
-    
-    public BigDecimal getEventPrice() {
-        return eventPrice;
-    }
-    //    }
-    //        this.eventDateTime = eventDateTime;
-    //    public EventDTO(LocalDateTime eventDateTime) {
-    //        return eventDateTime;
-    //    }
-    //
-//    public LocalDateTime getEventDateTime() {
-//    public void setEventDateTime(LocalDateTime eventDateTime) {
-//        this.eventDateTime = eventDateTime;
-//    }
 }
