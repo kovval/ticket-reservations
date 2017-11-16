762e507816feb0bd646380cb78791ab4a71488ab
michsz <michal.szagol@gmail.com>
2017-11-16 20:34:13 +0100

    added method for checking if seat with user-given name exists

--- src/main/java/com/github/java4wro/csvparser/repository/HallRepository.java
+++ src/main/java/com/github/java4wro/csvparser/repository/HallRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface HallRepository extends JpaRepository<Seat, Long> {
     Seat findOneBySeat(String seat);
+    boolean existsBySeat(String seat);
 }
