ab6786aa1b88444472b650afedfff75cbe9ea2f0
RENT-Komputer\RENT <jan.kisiel22@gmail.com>
2017-11-17 18:43:51 +0100

    GeneratorPDF and GeneratorQR compile with ticketUUID

--- src/main/java/com/github/java4wro/emailService/EmailSenderImpl.java
+++ src/main/java/com/github/java4wro/emailService/EmailSenderImpl.java
@@ -1,13 +1,9 @@
 package com.github.java4wro.emailService;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
@@ -56,7 +52,5 @@ public class EmailSenderImpl implements EmailSender {
         }
 
         this.javaMailSender.send(mail);
-
     }
-
 }
