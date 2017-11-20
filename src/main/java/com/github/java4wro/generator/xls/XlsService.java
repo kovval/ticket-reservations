9b6269f743fcec89434fac3a7a7d3d2e47f11dfc
Daniel <ddmirecki@gmail.com>
2017-11-20 18:27:36 +0100

    add XLS generator

---
+++ src/main/java/com/github/java4wro/generator/xls/XlsService.java
@@ -0,0 +1,60 @@
+package com.github.java4wro.generator.xls;
+
+import com.github.java4wro.generator.dto.EventDTO;
+import com.github.java4wro.generator.dto.TicketDTO;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class XlsService {
+
+
+    public HSSFWorkbook createXlsReport(int row, EventDTO eventDTO, List<TicketDTO> ticketDTOs) {
+
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet("Zestawienie");
+
+        List<HSSFRow> rows = new ArrayList<>();
+        rows.add(createHeader(sheet));
+        rows.addAll(createContent(sheet, row, eventDTO, ticketDTOs));
+
+        return workbook;
+    }
+
+    private HSSFRow createHeader(HSSFSheet sheet) {
+
+        final HSSFRow row = sheet.createRow(0);
+
+        row.createCell(0).setCellValue("Zestawienie");
+        row.createCell(1).setCellValue("Event Name");
+        row.createCell(2).setCellValue("Event Date");
+        row.createCell(3).setCellValue("Ticket Id");
+        row.createCell(4).setCellValue("Ticket Price");
+        return row;
+    }
+
+    private List<HSSFRow> createContent(HSSFSheet sheet, int r,EventDTO eventDTO, List<TicketDTO> ticketDTOs) {
+        List<HSSFRow> rows = new ArrayList<>();
+        int currentrow = r;
+        for (TicketDTO t :ticketDTOs) {
+            final HSSFRow row = sheet.createRow(currentrow);
+            rows.add(row);
+            row.createCell(1).setCellValue(eventDTO.getEventName());
+            row.createCell(2).setCellValue(eventDTO.getEventDateAndTime().toString());
+            row.createCell(3).setCellValue(t.getTicketId());
+            row.createCell(4).setCellValue(t.getPrice().toString());
+            currentrow++;
+        }
+        return rows;
+    }
+
+}
