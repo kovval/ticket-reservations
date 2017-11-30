b87bc271e90243d23fc1d2b3d9fe9f875ea5315d
Kovval-Komputer\Kovval <jan.kisiel22@gmail.com>
2017-11-30 08:45:52 +0100

    change PDFgenerator and add arial to use polish chars.

--- src/main/java/com/github/java4wro/generator/GeneratorPDF.java
+++ src/main/java/com/github/java4wro/generator/GeneratorPDF.java
@@ -8,12 +8,14 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType0Font;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.mail.MessagingException;
+import java.io.File;
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 
@@ -32,56 +34,59 @@ public class GeneratorPDF {
         PDPage page = new PDPage();
         document.addPage(page);
 
+
         GeneratorQR generatorQR = new GeneratorQR();
         generatorQR.generateQRCode(eventPdfDTO, ownerPdfDTO, ticketDTO);
-        String imageQR = ticketDTO.getTicketuuid() + ".jpg";
+        String imageQR = "QRCode.jpg";
 
-        PDFont font = PDType1Font.TIMES_ROMAN;
+        PDType0Font font = PDType0Font.load(document, new File("arial.ttf"));
+
 
         //content i obraz QR
         PDImageXObject image = PDImageXObject.createFromFile(imageQR, document);
         PDPageContentStream contentStream = new PDPageContentStream(document, page);
 
+
         contentStream.beginText();
         contentStream.setFont(font, 25);
-        contentStream.moveTextPositionByAmount(160, 750);
-        contentStream.drawString("NARODOWE FORUM MUZYKI ");
+        contentStream.newLineAtOffset(180, 750);
+        contentStream.showText("NARODOWE FORUM MUZYKI ");
         contentStream.endText();
 
         //Nazwa wydarzenia
         contentStream.beginText();
         contentStream.setFont(font, 14);
-        contentStream.moveTextPositionByAmount(100, 700);
-        contentStream.drawString("Event: " + eventPdfDTO.getEventName());
+        contentStream.newLineAtOffset(100, 700);
+        contentStream.showText("Event: " + eventPdfDTO.getEventName());
         contentStream.endText();
 
         //data
         contentStream.beginText();
         contentStream.setFont(font, 14);
-        contentStream.moveTextPositionByAmount(100, 650);
+        contentStream.newLineAtOffset(100, 650);
         String dataEvent = eventPdfDTO.getEventDateAndTime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
-        contentStream.drawString("Data: " + dataEvent);
+        contentStream.showText("Data: " + dataEvent);
         contentStream.endText();
 
         //cena wydarzenia
         contentStream.beginText();
         contentStream.setFont(font, 14);
-        contentStream.moveTextPositionByAmount(400, 600);
-        contentStream.drawString("Price: " + ticketDTO.getPrice());
+        contentStream.newLineAtOffset(400, 600);
+        contentStream.showText("Price: " + ticketDTO.getPrice());
         contentStream.endText();
 
         //Email
         contentStream.beginText();
         contentStream.setFont(font, 14);
-        contentStream.moveTextPositionByAmount(400, 700);
-        contentStream.drawString("Ticket for: " + ownerPdfDTO.getEmail());
+        contentStream.newLineAtOffset(400, 550);
+        contentStream.showText("Ticket for: " + ownerPdfDTO.getEmail());
         contentStream.endText();
 
         // Miejsce i Rząd
         contentStream.beginText();
         contentStream.setFont(font, 14);
-        contentStream.moveTextPositionByAmount(100, 600);
-        contentStream.drawString("seat: "
+        contentStream.newLineAtOffset(100, 600);
+        contentStream.showText("seat: "
                 + ticketDTO.getSeat()
                 + " row: "
                 + ticketDTO.getRow());
@@ -90,8 +95,8 @@ public class GeneratorPDF {
         //Id
         contentStream.beginText();
         contentStream.setFont(font, 14);
-        contentStream.moveTextPositionByAmount(400, 650);
-        contentStream.drawString("id: " + ticketDTO.getTicketId().hashCode());
+        contentStream.newLineAtOffset(400, 650);
+        contentStream.showText("id: " + ticketDTO.getTicketId().hashCode());
         contentStream.endText();
 
         //kod QR
@@ -102,10 +107,9 @@ public class GeneratorPDF {
         document.save(eventPdfDTO.getEventName() + ticketDTO.getTicketId().hashCode() + ".pdf");
         document.close();
 
-
         emailSender.sendEmail(ownerPdfDTO.getEmail(),
-                "Bilet", "Bilet w załączniku",
-                (eventPdfDTO.getEventName() + ticketDTO.getTicketuuid().hashCode() + ".pdf"));
-
+                "Bilet", "bilet w załączniku",
+                eventPdfDTO.getEventName() + ticketDTO.getTicketId().hashCode() + ".pdf");
     }
+
 }
